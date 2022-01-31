0ef5d6b31f15686cf2ba685e87929e28f75bb176
max8539 <maxyuen8539@gmail.com>
2022-01-31 21:19:37 +1100

    Update token checker

--- api/login.js
+++ api/login.js
@@ -6,29 +6,39 @@ const path = require("path");
 const crypto = require("crypto");
 const SETTINGS = JSON.parse(fs.readFileSync(path.join(__dirname,"/settings.json")));
 const jwt = require("jsonwebtoken");
-const { resourceLimits } = require("worker_threads");
 
 
 function tokenCheck (token) {
+    if (token == undefined) {
+        throw Error("400");
+    }
+    
     const USERS = JSON.parse(fs.readFileSync(path.join(__dirname,"/data/user-data.json")));
     let tokenData;
     let result = {
         loggedIn: false,
     }
+    
+    // Attempt to verify and decode token, return logged out result if it fails.
     try {
         tokenData = jwt.verify(token, SETTINGS.tokenSecretKey);
     } catch (err) {
         return result;
     }
+    // Check all token fields are present
     if (Object.keys(tokenData) != ["uname","createdTime"]) {
         return result;
     }
+    // Check that token timestamp meets general server rules
     if (tokenData.createdTime > Date.now() || 
     Date.now() - tokenData.createdTime > (SETTINGS.tokenExpiryDays * 86400)) {
         return result;
     }
+    // Check that username exists, and token timestamp is 
+    // greater than the user's earliest login time, and
+    // update return object if successful.
     USERS.users.forEach(function (user) {
-        if (user.uname == tokenData.uname && user.earliestLogin < tokenData.createdTime) {
+        if (user.uname == tokenData.uname && user.earliestLogin <= tokenData.createdTime) {
             result.loggedIn = true;
             result.uname = tokenData.uname;
         }
